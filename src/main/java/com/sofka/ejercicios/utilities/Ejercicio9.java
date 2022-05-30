0a224a3bae268ea86c09d3a0bcb06fb4c52588e5
certhakzu <pealveco@yahoo.com>
2022-05-30 13:38:41 -0500

    Ejercicio 9 TERMINADO

---
+++ src/main/java/com/sofka/ejercicios/utilities/Ejercicio9.java
@@ -0,0 +1,20 @@
+package com.sofka.ejercicios.utilities;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Ejercicio9 {
+
+    static final Logger logger = Logger.getLogger(String.valueOf(Ejercicio6.class));
+    static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String fraseInicial = "La sonrisa sera la mejor arma contra la tristeza"; //reemplazar 'a' con 'e'
+        String fraseAdicional;
+
+        logger.info("Escriba frase que quiere adicionar: ");
+        fraseAdicional = scanner.nextLine();
+        logger.log(Level.SEVERE, " {0}", fraseInicial.replace('a', 'e').concat(fraseAdicional));
+    }
+}
