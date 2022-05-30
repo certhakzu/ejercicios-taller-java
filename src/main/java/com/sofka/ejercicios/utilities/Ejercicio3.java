9542ccee8e463f83e66fbb5d8aed7fcf7ab606fc
certhakzu <pealveco@yahoo.com>
2022-05-30 09:55:26 -0500

    Ejercicio 3 TERMINADO

---
+++ src/main/java/com/sofka/ejercicios/utilities/Ejercicio3.java
@@ -0,0 +1,20 @@
+package com.sofka.ejercicios.utilities;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Ejercicio3 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Logger logger = Logger.getLogger(String.valueOf(Ejercicio1.class));
+    static double radio;
+    static double areaCirculo;
+
+    public static void main(String[] args) {
+        logger.info("Digite Radio: ");
+        radio = Double.parseDouble((scanner.nextLine()));
+        areaCirculo = Math.pow(Math.PI, radio);
+        logger.log(Level.INFO, "Área de Cirulo = {0}", areaCirculo);
+    }
+}
